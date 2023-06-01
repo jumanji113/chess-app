f76bfe695f8a924c0461f22000a2b59ddc2a2fce
Alex <udinkik@mail.ru>
2023-06-01 13:29:20 +0300

    first commit

---
+++ src/components/Chessboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Chessboard.css";
+
+const Chessboard = () => {
+    return (
+        <div>Chessboard i am chessboard</div>
+    )
+}
+
+export default Chessboard
